bf1bba697aca77c68ac82fb6faf0f63fa0f601eb
Saliou DIABATE <diabate.ci@gmail.com>
2020-10-10 23:15:08 +0000

    Clear livewire component

--- resources/views/livewire/alert.blade.php
+++ resources/views/livewire/alert.blade.php
@@ -1,47 +1,46 @@
 <div>
-
-@if($shown)
-    <div
-        x-data="{ show: true }"
-        x-init="setTimeout(() => { show = false; $wire.dismiss(); }, {{ ($toast['autoDismiss']) ? ($toast['duration'] != 0 ? $toast['duration'] : config('toast-notification.duration')) : config('toast-notification.longDuration') }})"
-        x-show="show"
-        x-transition:enter="transform ease-out duration-300 transition"
-        x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-        x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
-        x-transition:leave="transition ease-in duration-100"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-        class="w-full flex justify-start items-center z-10000 px-4 py-1 my-2 relative shadow-lg pointer-events-auto border-t-4 border-l-2 border-gray-200
-        @if($toast['type'] == 'danger') bg-danger text-white @endif
-        @if($toast['type'] == 'default') bg-gray-500 text-white @endif
-        @if($toast['type'] == 'info') bg-info text-white @endif
-        @if($toast['type'] == 'success') bg-success text-white @endif
-        @if($toast['type'] == 'warning') bg-warning text-white @endif"
-        role="alert">
-        @if($toast['type'] === 'default')
-            @include('toastnotification::icons.alerts.default')
-        @endif
-        @if($toast['type'] === 'success')
-            @include('toastnotification::icons.alerts.success')
-        @endif
-        @if($toast['type'] === 'warning')
-            @include('toastnotification::icons.alerts.warning')
-        @endif
-        @if($toast['type'] === 'info')
-            @include('toastnotification::icons.alerts.info')
-        @endif
-        @if($toast['type'] === 'danger')
-            @include('toastnotification::icons.alerts.danger')
-        @endif
-        <div class="flex justify-center items-center px-4 sm:inline">{{ $toast['description'] }}</div>
-        @if (! $toast['autoDismiss'])
-            <span class="absolute top-0 bottom-0 right-0 px-4 py-3" x-on:click="show = false; $wire.dismiss();">
-                <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <title>Close</title>
-                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                </svg>
-            </span>
-        @endif
-    </div>
-@endif
+    @if($shown)
+        <div
+            x-data="{ show: true }"
+            x-init="setTimeout(() => { show = false; $wire.dismiss(); }, {{ ($toast['autoDismiss']) ? ($toast['duration'] != 0 ? $toast['duration'] : config('toast-notification.duration')) : config('toast-notification.longDuration') }})"
+            x-show="show"
+            x-transition:enter="transform ease-out duration-300 transition"
+            x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
+            x-transition:leave="transition ease-in duration-100"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="w-full flex justify-start items-center z-10000 px-4 py-1 my-2 relative shadow-lg pointer-events-auto border-t-4 border-l-2 border-gray-200
+            @if($toast['type'] == 'danger') bg-danger text-white @endif
+            @if($toast['type'] == 'default') bg-gray-500 text-white @endif
+            @if($toast['type'] == 'info') bg-info text-white @endif
+            @if($toast['type'] == 'success') bg-success text-white @endif
+            @if($toast['type'] == 'warning') bg-warning text-white @endif"
+            role="alert">
+            @if($toast['type'] === 'default')
+                @include('toastnotification::icons.alerts.default')
+            @endif
+            @if($toast['type'] === 'success')
+                @include('toastnotification::icons.alerts.success')
+            @endif
+            @if($toast['type'] === 'warning')
+                @include('toastnotification::icons.alerts.warning')
+            @endif
+            @if($toast['type'] === 'info')
+                @include('toastnotification::icons.alerts.info')
+            @endif
+            @if($toast['type'] === 'danger')
+                @include('toastnotification::icons.alerts.danger')
+            @endif
+            <div class="flex justify-center items-center px-4 sm:inline">{{ $toast['description'] }}</div>
+            @if (! $toast['autoDismiss'])
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3" x-on:click="show = false; $wire.dismiss();">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <title>Close</title>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                </span>
+            @endif
+        </div>
+    @endif
 </div>
--- resources/views/toast.blade.php
+++ resources/views/toast.blade.php
@@ -1,6 +1,7 @@
-@if(session()->has('ndoto_toast_notification') && session()->get('ndoto_toast_notification.model') == 'toast')
-    <div class="fixed inset-0 z-10000 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
-        <div
+<div>
+    @if(session()->has('ndoto_toast_notification') && session()->get('ndoto_toast_notification.model') == 'toast')
+        <div class="fixed inset-0 z-10000 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+            <div
                 x-data="{ show: true }"
                 x-init="setTimeout(() => { show = false }, {{ (session()->get('ndoto_toast_notification.autoDismiss')) ? (session()->get('ndoto_toast_notification.duration') != 0 ? session()->get('ndoto_toast_notification.duration') : config('toast-notification.duration')) : config('toast-notification.longDuration') }})"
                 x-show="show"
@@ -11,48 +12,49 @@
                 x-transition:leave-start="opacity-100"
                 x-transition:leave-end="opacity-0"
                 class="max-w-sm w-full shadow-lg rounded-lg pointer-events-auto border-t-4 border-l-2 border-gray-300 bg-white">
-            <div class="relative rounded-lg shadow-xs overflow-hidden">
-                <div class="p-4">
-                    <div class="flex items-start">
-                        @if(session()->get('ndoto_toast_notification.type') === 'default')
-                            @include('toastnotification::icons.default')
-                        @endif
-                        @if(session()->get('ndoto_toast_notification.type') === 'success')
-                            @include('toastnotification::icons.success')
-                        @endif
-                        @if(session()->get('ndoto_toast_notification.type') === 'warning')
-                            @include('toastnotification::icons.warning')
-                        @endif
-                        @if(session()->get('ndoto_toast_notification.type') === 'info')
-                            @include('toastnotification::icons.info')
-                        @endif
-                        @if(session()->get('ndoto_toast_notification.type') === 'danger')
-                            @include('toastnotification::icons.danger')
-                        @endif
-                        <div class="ml-4 w-0 flex-1">
-                            <p class="text-base leading-5 font-medium capitalize
-                                @if(session()->get('ndoto_toast_notification.type') === 'danger') text-danger @endif
-                            @if(session()->get('ndoto_toast_notification.type') === 'default') text-gray-500 @endif
-                            @if(session()->get('ndoto_toast_notification.type') === 'info') text-info @endif
-                            @if(session()->get('ndoto_toast_notification.type') === 'success') text-success @endif
-                            @if(session()->get('ndoto_toast_notification.type') === 'warning') text-warning @endif
-                                    ">
-                                {{ session()->get('ndoto_toast_notification.title') ?? session()->get('ndoto_toast_notification.type') }}
-                            </p>
-                            <p class="mt-1 text-sm leading-5 text-gray-500">
-                                {{ session()->get('ndoto_toast_notification.description') }}
-                            </p>
-                        </div>
-                        @if (! session()->get('ndoto_toast_notification.autoDismiss'))
-                            <div class="ml-4 flex-shrink-0 flex">
-                                <button x-on:click="show = false" class="inline-flex text-gray-400 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150">
-                                    @include('toastnotification::icons.close')
-                                </button>
+                <div class="relative rounded-lg shadow-xs overflow-hidden">
+                    <div class="p-4">
+                        <div class="flex items-start">
+                            @if(session()->get('ndoto_toast_notification.type') === 'default')
+                                @include('toastnotification::icons.default')
+                            @endif
+                            @if(session()->get('ndoto_toast_notification.type') === 'success')
+                                @include('toastnotification::icons.success')
+                            @endif
+                            @if(session()->get('ndoto_toast_notification.type') === 'warning')
+                                @include('toastnotification::icons.warning')
+                            @endif
+                            @if(session()->get('ndoto_toast_notification.type') === 'info')
+                                @include('toastnotification::icons.info')
+                            @endif
+                            @if(session()->get('ndoto_toast_notification.type') === 'danger')
+                                @include('toastnotification::icons.danger')
+                            @endif
+                            <div class="ml-4 w-0 flex-1">
+                                <p class="text-base leading-5 font-medium capitalize
+                                    @if(session()->get('ndoto_toast_notification.type') === 'danger') text-danger @endif
+                                @if(session()->get('ndoto_toast_notification.type') === 'default') text-gray-500 @endif
+                                @if(session()->get('ndoto_toast_notification.type') === 'info') text-info @endif
+                                @if(session()->get('ndoto_toast_notification.type') === 'success') text-success @endif
+                                @if(session()->get('ndoto_toast_notification.type') === 'warning') text-warning @endif
+                                        ">
+                                    {{ session()->get('ndoto_toast_notification.title') ?? session()->get('ndoto_toast_notification.type') }}
+                                </p>
+                                <p class="mt-1 text-sm leading-5 text-gray-500">
+                                    {{ session()->get('ndoto_toast_notification.description') }}
+                                </p>
                             </div>
-                        @endif
+                            @if (! session()->get('ndoto_toast_notification.autoDismiss'))
+                                <div class="ml-4 flex-shrink-0 flex">
+                                    <button x-on:click="show = false" class="inline-flex text-gray-400 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150">
+                                        @include('toastnotification::icons.close')
+                                    </button>
+                                </div>
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endif
+    @endif
+</div>
